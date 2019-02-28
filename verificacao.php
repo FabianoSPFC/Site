bde35f89f094c2e4fe3712075cc89b3b93c827d8
FabianoSPFC <programador.workspace@gmail.com>
2019-02-28 00:34:35 -0300

    Modificação no arquivo

--- verificacao.php
+++ verificacao.php
@@ -9,8 +9,17 @@
 <body>
     <?php
         if($_SERVER["REQUEST_METHOD"] == "POST"){
-            $questao = $_POST["questao"]
+        	
+            $questao = $_POST["questao"];
+			$letra_a = $_POST["letra A"];       
+			$letra_b = $_POST["letra B"];      
+			$letra_c = $_POST["letra C"];       
+			$letra_d = $_POST["letra D"];        
+			$letra_e = $_POST["letra E"]; 
+			$resposta = $_POST["resposta"];
+			echo $questao,$letra_a,$letra_b,$letra_c,$letra_d,$letra_e;
         }   
+
     ?>
 </body>
 </html>
