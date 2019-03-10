921fb68236fcb0f624f23b2e851b178ca7a4d85a
FabianoSPFC <programador.workspace@gmail.com>
2019-03-10 13:01:07 -0300

    Cronometro feito em js

---
+++ cronometro.js
@@ -0,0 +1,17 @@
+var intervalo;
+
+function tempo(op) {	
+	var s = 1;
+	var m = 0;
+	var h = 0;
+	intervalo = window.setInterval(function() {
+		if (s == 60) { m++; s = 0; }
+		if (m == 60) { h++; s = 0; m = 0; }
+		if (h < 10) document.getElementById("hora").innerHTML = "0" + h + "h"; else document.getElementById("hora").innerHTML = h + "h";
+		if (s < 10) document.getElementById("segundo").innerHTML = "0" + s + "s"; else document.getElementById("segundo").innerHTML = s + "s";
+		if (m < 10) document.getElementById("minuto").innerHTML = "0" + m + "m"; else document.getElementById("minuto").innerHTML = m + "m";		
+		s++;
+	},1000);
+}
+
+window.onload=tempo;
