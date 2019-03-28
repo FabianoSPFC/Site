1bcbb6a0c371f452e915771289adeb448030a139
FabianoSPFC <programador.workspace@gmail.com>
2019-03-28 12:30:34 -0300

    Falta terminar de incrementar o código, comparando com o que o usuario colocou

---
+++ pin.js
@@ -0,0 +1,7 @@
+<script type="text/javascript"> 
+    var min=1000; 
+    var max=9999;  
+    var random =Math.floor(Math.random() * (+max - +min)) + +min; 
+    document.write("Random Number Generated : " + random );  
+    console.log(random)
+</script> 
